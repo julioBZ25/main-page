a68920982f986f11f03202212060cd039868cfb4
Julio <jbzuloaga@gmail.com>
2024-10-12 16:16:42 -0500

    refactor: add env variables type

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,11 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+  readonly REPOSITORY_NAME: string;
+  readonly ACCESS_TOKEN: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
